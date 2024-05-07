f117ff39ca4e2323464f0f601bb28d1ebda7115e
Babayka93 <splintercell1993@gmail.com>
2024-05-07 13:09:35 +0300

    new

--- plottingg.py
+++ plottingg.py
@@ -1,9 +1,17 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# Создаем новые столбцы для каждого уникального значения
 unique_values = data['whoAmI'].unique()
-
-
 for value in unique_values:
-    data[value] = (data['whoAmI'] == value).astype(int)
+    data[f'is_{value}'] = (data['whoAmI'] == value).astype(int)
 
-data = data.drop('whoAmI', axis=1)  
+# Удаляем исходный столбец, если нужно
+data.drop(columns=['whoAmI'], inplace=True)
 
-data.head()
+print(data.head())
